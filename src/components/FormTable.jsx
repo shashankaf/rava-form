df402f6118a4eced34cdb64ac891c70f51e92bcb
Shalaw Fatah <shalaw.fatah@yahoo.com>
2024-04-29 16:47:31 +0300

    self issue on netlify

--- src/components/FormTable.jsx
+++ src/components/FormTable.jsx
@@ -8,33 +8,33 @@ const FormTable = ({student, teachers}) => {
       <table  className="table-auto border border-collapse border-gray-400 my-4 m-auto">
         <tbody>
           <tr  className="bg-white text-right">
-            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.class?.title}</td>
+            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student?.class?.title}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">پۆل</td>
-            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.name}</td>
+            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student?.name}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">ناوی بەشداربوو</td>
           </tr>
           <tr className="bg-white text-right">
-            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.ragaz?.title}</td>
+            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student?.ragaz?.title}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">رەگەز</td>
-            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.school}</td>
+            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student?.school}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">خوێندنگە</td>
           </tr>
           <tr className="bg-white text-right">
-            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.phone}</td>
+            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student?.phone}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">ژمارەی تەلەفۆن</td>
-            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.blood?.title}</td>
+            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student?.blood?.title}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">گروپی خوێن</td>
           </tr>
           <tr className="bg-white text-right">
-            <td cstyle={{fontFamily: 'rudaw'}} lassName="border px-4 py-2">{student.travel?.title}</td>
+            <td cstyle={{fontFamily: 'rudaw'}} lassName="border px-4 py-2">{student?.travel?.title}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">هاتووچۆ</td>
-            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.address}</td>
+            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student?.address}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">ناونیشان</td>
           </tr>
           <tr className="bg-white text-right">
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.second_pay}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">بڕی واسڵکراو \ ٢</td>
-            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.first_pay}</td>
+            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student?.first_pay}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">بڕی واسڵکراو \ ١</td>
           </tr>
           <tr className="bg-white text-right">
@@ -44,7 +44,7 @@ const FormTable = ({student, teachers}) => {
                 </li>
               ))}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">مامۆستای خوازراو</td>
-            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student.health}</td>
+            <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">{student?.health}</td>
             <td style={{fontFamily: 'rudaw'}} className="border px-4 py-2">باری تەندروستی</td>
           </tr>
         </tbody>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Home() {
   return (
