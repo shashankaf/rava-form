d248c71b7013e549a811983332b7fdfe74344f1d
Shalaw Fatah <shalaw.fatah@yahoo.com>
2024-05-03 21:19:40 +0300

    sidebar issue solved

--- src/components/DashCmp.jsx
+++ src/components/DashCmp.jsx
@@ -1,42 +1,67 @@
-
-import React from 'react';
-import Link from 'next/link';
+import React, { useState } from "react";
+import Link from "next/link";
 import localFont from "next/font/local";
-import { supabase } from '../lib/supabase';
-import { useRouter } from 'next/router';
+import { supabase } from "../lib/supabase";
+import { useRouter } from "next/router";
 
 const shasenem = localFont({ src: "../pages/fonts/shasenem.ttf" });
 
-const Dashboard = ({children}) => {
-const router = useRouter()
+const Dashboard = ({ children }) => {
+  const router = useRouter();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   const signOut = async () => {
     const { error } = await supabase.auth.signOut();
     if (error) {
       setErrMsg("ببورە هەڵەیەک روویداوە");
     } else {
-      router.reload()
+      router.reload();
     }
   };
 
   return (
-    <div dir="rtl" className="flex">
-      <div  className="bg-gray-100 text-gray-900 w-1/5 min-h-screen flex flex-col rounded-tl-3xl rounded-bl-3xl">
-        <Link className={`${shasenem.className} block py-4 px-6 hover:bg-gray-200 transition-400`} href="/dashboard/students">
+    <div dir="rtl" className="flex flex-col md:flex-row">
+      <button
+        className="md:hidden bg-gray-100 text-gray-900 py-2 px-4 rounded-tl-3xl rounded-bl-3xl"
+        onClick={toggleMenu}
+      >
+        پەڕەکان ↓
+      </button>
+      <div
+        className={`${
+          menuOpen ? "block" : "hidden md:block"
+        } bg-gray-100 text-gray-900 w-full md:w-1/5 min-h-screen flex flex-col rounded-tl-3xl rounded-bl-3xl`}
+      >
+        <Link
+          className={`${shasenem.className} block py-4 px-6 hover:bg-gray-200 transition-400`}
+          href="/dashboard/students"
+        >
           خوێندکاران
         </Link>
-        <Link className={`${shasenem.className} block py-4 px-6 hover:bg-gray-200 transition-400`}  href="/dashboard/teachers">
+        <Link
+          className={`${shasenem.className} block py-4 px-6 hover:bg-gray-200 transition-400`}
+          href="/dashboard/teachers"
+        >
           مامۆستایان
         </Link>
-        <Link className={`${shasenem.className} block py-4 px-6 hover:bg-gray-200 transition-400`} href="/dashboard/cohorts">
+        <Link
+          className={`${shasenem.className} block py-4 px-6 hover:bg-gray-200 transition-400`}
+          href="/dashboard/cohorts"
+        >
           خولەکان
         </Link>
-        <div className={`${shasenem.className} cursor-pointer block py-4 px-6 hover:bg-gray-300 transition-400`} onClick={signOut}>
-          دەرچوون لە ئەکاونت        
+        <div
+          className={`${shasenem.className} cursor-pointer block py-4 px-6 hover:bg-gray-300 transition-400`}
+          onClick={signOut}
+        >
+          دەرچوون لە ئەکاونت
         </div>
       </div>
-      <div className="flex-1 p-4">
-        {children}
-      </div>
+      <div className="flex-1 p-4">{children}</div>
     </div>
   );
 };
