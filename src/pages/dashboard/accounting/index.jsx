66fae1d1471aeb313a1d90122a8ff5a5d5b7421a
Shalaw Fatah <shalaw.fatah@yahoo.com>
2024-05-07 19:11:01 +0300

    income and expense started

--- src/pages/dashboard/accounting/index.jsx
+++ src/pages/dashboard/accounting/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AuthLayout from "../../../components/AuthLayout";
 import DashCmp from "../../../components/DashCmp";
-import IncomeForm from "../../../components/accounting/IncomeForm";
 import Head from "next/head";
 import AllAccounting from "../../../components/accounting/AllAccounting";
+import GeneralTable from "../../../components/GeneralTable";
 
 const Expenses = () => {
+  const items = [{id:1, income: 800, student: {name: 'hi'}, course: {title: 'bye'}}]
+  const labels = [{id:1, title: "داهات"}, {id: 2, title: "خوێندکار"}, {id: 3, title: "خول"}]
   return (
     <AuthLayout>
       <Head>
@@ -13,6 +15,15 @@ const Expenses = () => {
       </Head>
       <DashCmp>
         <AllAccounting />
+        <GeneralTable 
+          title={"داهات"}
+          createRoute="dashboard/accounting/create"
+          editRoute="dashboard/accounting/edit"
+          readRoute="dashboard/accounting/read"
+          items={items}
+          table="income"
+          labels={labels}
+        />
       </DashCmp>
     </AuthLayout>
   );
